30672ece943400bd5cb670d50c5afbf0330a599b
Swanand Deshpande <swanand.deshpande@mobisoftinfotech.com>
2018-03-07 18:13:20 +0530

    Added : AlertDialog on back pressed.

--- app/src/main/java/com/whatsap/statussaver/activity/LandingActivity.java
+++ app/src/main/java/com/whatsap/statussaver/activity/LandingActivity.java
@@ -1,12 +1,16 @@
 package com.whatsap.statussaver.activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 
 import com.whatsap.statussaver.R;
@@ -61,5 +65,51 @@ public class LandingActivity extends AppCompatActivity {
 
         viewPager.setAdapter(pagerAdapter);
 
+    }
+
+    /**
+     * Exit the app on back pressed
+     **/
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+            exitApp();
+
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    private void exitApp() {
+
+        AlertDialog alertBox = new AlertDialog.Builder(activity)
+                .setMessage(activity.getResources().getString(R.string.msg_exit_app))
+                .setPositiveButton(activity.getResources().getString(R.string.yes),
+                        new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                        finish();
+
+                    }
+                })
+                .setNegativeButton(activity.getResources().getString(R.string.no),
+                        new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+
+                }).create();
+
+        alertBox.show();
+
+        alertBox.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(activity, R.color.colorPrimaryDark));
+        alertBox.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(activity, R.color.colorPrimaryDark));
+        alertBox.setCancelable(false);
     }
 }
